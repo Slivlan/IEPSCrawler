cccef6655e8cc576c3e7eeaa4cc112ff6175480f
Luka Zakšek <luka.zaksek@gmail.com>
2020-03-30 14:39:29 +0200

    Added code for parsing images and links from webpage.

--- crawler.py
+++ crawler.py
@@ -1,14 +1,18 @@
 import urllib
-import selenium
+from selenium import webdriver
 from urllib import robotparser
 import psycopg2
 import threading
 import concurrent.futures
+from bs4 import BeautifulSoup
 
 domains = ["gov.si", "evem.gov.si", "e-uprava.gov.si", "e-prostor.gov.si"]
 request_rate = 5
 user_agent = "fri-ieps-nasagrupa"
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+driver = webdriver.Chrome(chrome_options = chrome_options)
 def put_site_in_db(domain):
 
     try:
@@ -57,9 +61,35 @@ def get_robots_sitemap_data(url):
 
     return (robots_data, sitemap_data)
 
+"""
+	Detect images and next links
+"""
+def get_images_links(driver, domain):
+	driver.get(domain)
+	print("Domain: ", domain)
+	#print(driver.page_source)
+	page_content = driver.page_source
+	# Html_file= open("tmp_{}".format(domain),"w")
+	# Html_file.write(page_content)
+	# Html_file.close()
+	images = []
+	links = []
+	page = BeautifulSoup(page_content, 'html.parser')
+	imgs = page.findAll('img')
+	for img in imgs:
+		src = img.get_attribute('src')
+		images.append(src)
+	hyperlinks = page.findAll('a')
+	for hyperlink in hyperlinks:
+		href = hyperlink.get_attribute('href')
+		links.append(href)
+	print("------------")
+	driver.quit()
+
 lock = threading.Lock()
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
     print(f"\n ... executing workers ...\n")
     for domain in domains:
         executor.submit(put_site_in_db, domain)
+        executor.submit(get_images_links, driver, 'https://'+domain)
