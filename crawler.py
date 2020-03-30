0e45db54724e22fb7cc9c7a6862199e666195988
Lovro Vražič <lovrovrazic@Lovros-MacBook-Pro.local>
2020-03-30 21:24:04 +0200

    robots and sitemap content saving to db

--- crawler.py
+++ crawler.py
@@ -7,20 +7,21 @@ import concurrent.futures
 from bs4 import BeautifulSoup
 
 domains = ["gov.si", "evem.gov.si", "e-uprava.gov.si", "e-prostor.gov.si"]
-request_rate = 5
+request_rate_sec = 5
 user_agent = "fri-ieps-nasagrupa"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('headless')
 driver = webdriver.Chrome(chrome_options = chrome_options)
+
 def put_site_in_db(domain):
 
     try:
+        robots_sitemap_data = get_robots_sitemap_data(domain)
+
         conn = psycopg2.connect("host=167.71.67.220 dbname=crawler user=crawler password=tojegeslo")
         conn.autocommit = True
         cur = conn.cursor()
-        url = "http://{}/robots.txt".format(domain)
-        robots_sitemap_data = get_robots_sitemap_data(url)
         cur.execute("SELECT domain FROM crawldb.site WHERE domain = %s", (domain,))
         rows = cur.fetchall()
         if not rows:
@@ -33,31 +34,30 @@ def put_site_in_db(domain):
         cur.close()
         conn.close()
 
-def get_robots_sitemap_data(url):
-    #TODO: return (sitemap_data, robots_data)
-
+def get_robots_sitemap_data(domain):
+    url = "http://{}/robots.txt".format(domain)
     rp = urllib.robotparser.RobotFileParser(url=url)
     rp.read()
-    # if rp.request_rate() > 5:
-    #     request_rate = rp.request_rate(user_agent)
 
-    s = rp.site_maps()
+    try:
+        request = urllib.request.Request(url, headers={'User-Agent': user_agent})
+        with urllib.request.urlopen(request) as response:
+            robots_data = response.read().decode("utf-8")
+    except Exception as e:
+        robots_data = None
 
-    request = urllib.request.Request(
-        url,
-        headers={'User-Agent': user_agent}
-    )
+    try:
+        s = rp.site_maps()
+        if s:
+            s = s[0]
+            request = urllib.request.Request(s, headers={'User-Agent': user_agent})
 
-    with urllib.request.urlopen(request) as response:
-        robots_data = response.read().decode("utf-8")
-
-    request = urllib.request.Request(
-        s,
-        headers={'User-Agent': user_agent}
-    )
-
-    with urllib.request.urlopen(request) as response:
-        sitemap_data = response.read().decode("utf-8")
+            with urllib.request.urlopen(request) as response:
+                sitemap_data = response.read().decode("utf-8")
+        else:
+            sitemap_data = None
+    except Exception as e:
+        sitemap_data = None
 
     return (robots_data, sitemap_data)
 
@@ -93,3 +93,6 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
     for domain in domains:
         executor.submit(put_site_in_db, domain)
         executor.submit(get_images_links, driver, 'https://'+domain)
+    # for domain in domains:
+    #     put_site_in_db(domain)
+    #     get_images_links(driver, 'https://' + domain)
